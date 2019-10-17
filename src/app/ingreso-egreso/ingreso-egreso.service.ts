421b602b907c28a5845341835d93b6420ac7c0ac
Sebastian <sebastian.valencian@gmail.com>
2019-10-16 20:24:26 -0500

    [Feat]Obtenemos la info de los items de firebase, y el uid de los items

--- src/app/ingreso-egreso/ingreso-egreso.service.ts
+++ src/app/ingreso-egreso/ingreso-egreso.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
 import { IngresoEgresoModel } from './ingreso-egreso.model';
 import { AuthService } from '../auth/auth.service';
+import { filter, map } from 'rxjs/operators';
+import { SetItemsAction } from './ingreso-egreso.actions';
+import { Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IngresoEgresoService {
 
+  ingresoEgresoListenerSubcription: Subscription = new Subscription();
+  ingresoEgresoItemsSubcription: Subscription = new Subscription();
 
-  constructor(private afDB: AngularFirestore, public authService: AuthService) { }
+  constructor(private afDB: AngularFirestore, public authService: AuthService, private store: Store<AppState>) { }
 
   // Grabamos a Firebase y creamos una nueva collection
   crearIngresoEgreso(ingresoEgreso: IngresoEgresoModel) {
@@ -17,9 +24,47 @@ export class IngresoEgresoService {
     const user = this.authService.getUsuario();
 
     return this.afDB
-            .doc(`${user.uid}/ingresos-egresos`)
-            .collection('items')
-            .add({...ingresoEgreso});
+      .doc(`${user.uid}/ingresos-egresos`)
+      .collection('items')
+      .add({ ...ingresoEgreso });
+  }
+
+  // Esucha cualquier cambio de la collection
+  initIngresoEgresoListener() {
+
+    // get uid
+    this.ingresoEgresoListenerSubcription = this.store.select('auth').pipe(
+      // transformar en algo diferente, filtra por una condicion, solo llega al subscribe si se cumple la condicion
+      filter(auth => auth.user != null)
+    ).subscribe(auth => {
+      this.ingresoEgresoItems(auth.user.uid);
+    });
+
+  }
+
+  private ingresoEgresoItems(uid: string) {
+    this.ingresoEgresoItemsSubcription = this.afDB.collection(`${uid}/ingresos-egresos/items`)
+      .snapshotChanges()
+      .pipe(
+        map(docData => {
+          return docData.map(doc => {
+            return {
+              uid: doc.payload.doc.id,
+              ...doc.payload.doc.data()
+              // monto: doc.payload.doc.data().monto
+            };
+          });
+        })
+      )
+      .subscribe((coleccion: any) => { // IngresoEgresoModel[]
+        console.log(coleccion);
+        this.store.dispatch(new SetItemsAction(coleccion));
+      });
+  }
+
+  cancelarSubscriptions() {
+    this.ingresoEgresoListenerSubcription.unsubscribe();
+    this.ingresoEgresoItemsSubcription.unsubscribe();
   }
 
 }
