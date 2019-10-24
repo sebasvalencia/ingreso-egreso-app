eecd1acce5f113894754869852064362b29d3811
Sebastian <sebastian.valencian@gmail.com>
2019-10-24 18:47:01 -0500

    [Feat]Listar lotos los ingresos y egresos

--- src/app/ingreso-egreso/detalle/detalle.component.ts
+++ src/app/ingreso-egreso/detalle/detalle.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { IngresoEgresoModel } from '../ingreso-egreso.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.component.html',
   styles: []
 })
-export class DetalleComponent implements OnInit {
+export class DetalleComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+
+  items: IngresoEgresoModel[];
+  subscription: Subscription = new Subscription();
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.subscription = this.store.select('ingresoEgreso').subscribe(ingresoEgreso => {
+      console.log('Detalle ingresoEgreso ', ingresoEgreso.items);
+      this.items = ingresoEgreso.items;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+
+  borrarItem(uid: string) {
+    console.log(uid);
   }
 
 }
